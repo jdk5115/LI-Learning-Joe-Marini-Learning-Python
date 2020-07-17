0e4334f829ea5f4cea468c1e5a353d45f5276742
Jonathan Kelly <jdk5115@gmail.com>
2020-07-17 16:24:57 -0400

    update

--- Exercise Files/Chapter 3/03_05/zipfile_start.py
+++ Exercise Files/Chapter 3/03_05/zipfile_start.py
@@ -3,13 +3,19 @@ import zipfile
 
 
 # TODO: Create a new ZIP archive
-
+# zfile = zipfile.ZipFile("archive.zip","w")
+# zfile.write("file1.txt")
+# zfile.write("file2.txt")
+# zfile.write("file3.txt")
+# zfile.close()
 
 # TODO: Check validity of the file
-
+# print(zipfile.is_zipfile("archive.zip"))
 
 # TODO: Read the properties of a ZIP archive
-
+zfile = zipfile.ZipFile("archive.zip","r")
+print(zfile.namelist())
+print(zfile.infolist)
 
 # TODO: Read the properties of ZIP contents
 
